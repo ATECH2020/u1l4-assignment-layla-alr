c2c5c4d0a8abb1745bef788ece61185fe78a7065
Layla Al-Rashed <layla.1219282@nv.ccsd.net>
2020-09-07 03:16:13 +0000

    Completed Timestamps

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -14,11 +14,21 @@ class Timestamps {
         int minutes2 = scanner.nextInt();
         int seconds2 = scanner.nextInt();
 
-
         /*
          *  your code goes here
          */
 
+         int deltaH= hours2-hours1;//difference btw 1st and second hour
+         int deltaM= minutes2-minutes1;//diff between 1st and second min
+         int deltaS= seconds2-seconds1;//diff between 1st and second sec
+
+        deltaH*=3600;// converting the difference of the hours to seconds
+
+        deltaM*=60;//converting the difference of the minutes to seconds
+        
+        //deltaS doesn't need to be multiplied bc it's already in seconds
+
+        System.out.print(deltaH+deltaM+deltaS);//printing the sum of all the seconds
 
         // closing the scanner object
         scanner.close();
