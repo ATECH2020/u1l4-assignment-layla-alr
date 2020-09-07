777f45d92801ad4b8207ac3ecb154ceaed412a7e
layla-alr <70871078+layla-alr@users.noreply.github.com>
2020-09-06 22:05:49 -0700

    Completed Analog clock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,11 +7,14 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
+        int inputDegree= scanner.nextInt();
+        
+        int hour= inputDegree/30;//30 degrees in an hour, finds the hour
+        int minDegree= inputDegree -(hour*30);//finds the leftover degrees after the hour mark 
+         minDegree= minDegree*2;// multiplies leftover degrees by 2 since there are 2 minutes/degree in hour hand
+        minDegree= minDegree*6;//multiplies those degrees by 6 since there are 6 degrees/minute in minute hand
 
-
-        /*
-         *  your code goes here
-         */
+        System.out.print(minDegree);
 
         // closing the scanner object
         scanner.close();
