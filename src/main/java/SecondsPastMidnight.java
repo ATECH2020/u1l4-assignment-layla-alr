5c27f148ce83cc87d2361bc623fdaea7db55cd71
Layla Al-Rashed <layla.1219282@nv.ccsd.net>
2020-09-07 03:02:07 +0000

    Completed SecondsPastMidnight

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
-class SecondsPastMidnight {
-    public static void main(String[] args) {
+class SecondsPastMidnight 
+{
+    public static void main(String[] args) 
+    {
 
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
@@ -10,8 +12,9 @@ class SecondsPastMidnight {
         int number = scanner.nextInt();
 
         //Your code goes here
-
-
+        int hours = number/3600;//3600 is the amount of seconds in an hour
+        int mins = number/60;//60 is the amount of seconds in a minute
+        System.out.print(hours+" "+mins);//displays the hours and minutes past since midnight
         // closing the scanner object
         scanner.close();
     }
